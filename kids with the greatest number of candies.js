bc645f3bae2ae487dee37070af7a6b111f7b828a
Mareboina Ravi <51162616+MareboinaRavi@users.noreply.github.com>
2025-07-23 18:49:29 +0530

    Create kids with the greatest number of candies.js

---
+++ kids with the greatest number of candies.js
@@ -0,0 +1,15 @@
+const arr = [2, 3, 5, 1, 3];
+const extraC = 3;
+let greatNo = 0;
+let result = [];
+for (let i = 0; i < arr.length; i++) {
+  const item = arr[i];
+  if (item > greatNo) {
+    greatNo = item;
+  }
+}
+for (let i = 0; i < arr.length; i++) {
+  const item = arr[i];
+  result.push(item + extraC >= greatNo);
+}
+console.log('text', result);
