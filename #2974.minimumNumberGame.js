11edfe0b4f23aa42d55538820d0ced204e4e99fd
Mareboina Ravi <51162616+MareboinaRavi@users.noreply.github.com>
2026-05-03 13:39:26 +0530

    Create #2974.minimumNumberGame.js

---
+++ #2974.minimumNumberGame.js
@@ -0,0 +1,27 @@
+var numberGame = function (nums) {
+    nums.sort((a, b) => a - b); // step 1: sort
+    let arr = [];
+
+    for (let i = 0; i < nums.length; i += 2) {
+        arr.push(nums[i + 1]); // Bob
+        arr.push(nums[i]);     // Alice
+    }
+
+    return arr;
+};
+
+console.log(numberGame([5,4,2,3])); // [3,2,5,4]
+
+----------------------------------------------------------------------
+// 👉 Slightly better than your version:
+// No extra array
+// In-place swap
+var numberGame = function (nums) {
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length; i += 2) {
+        [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
+    }
+
+    return nums;
+};
